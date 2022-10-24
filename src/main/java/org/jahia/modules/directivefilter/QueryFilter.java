71103b512fb36eb9333197f62c59f46bc3dcae6d
Alex Karmanov <alexandrk_@hotmail.com>
2022-10-24 08:04:20 -0400

    BACKLOG-20182 Update package name, add signature

--- src/main/java/org/jahia/modules/directivefilter/QueryFilter.java
+++ src/main/java/org/jahia/modules/directivefilter/QueryFilter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.foo.modules.directivefilter;
+package org.jahia.modules.directivefilter;
 
 import org.osgi.service.component.annotations.Component;
 
